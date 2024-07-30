b5f88617b445f113f02a099f0f5b5522cb8a9fbb
prisakura <leon.prisacaru@gmail.com>
2024-07-31 02:58:13 +0300

    rus ASCII table list FIO string

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-    //todo Напишите код который распечатает букву и ее код для всех букв русского алфавита формат вывода:
-    // А-123
-    // Б-321...
-    // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
+
     public static void main(String[] args) {
-        //todo код писать тут
+        for (char a = 'а'; a<= 'я';a++) {
+            System.out.println(Character.toUpperCase(a)+" - "+(int)Character.toUpperCase(a));
+            System.out.println(a+" - "+(int)a);
+        }
     }
 }
--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+    public static final String VAL_VAL = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя-";
+
+    private static String f,i,o;
+
+    private static boolean valString(String s){
+        int spaceCount = 0;
+        for (int i=0; i<s.length();i++)
+            if (!VAL_VAL.contains(String.valueOf(s.charAt(i))))
+                if(s.charAt(i)==' ')
+                   if(spaceCount+1>2) return false; else spaceCount++;
+                 else
+                     return false;
+        return spaceCount==2;
     }
+
+    private static void getFIO(String s){
+    f=s.substring(0,s.indexOf(' ')).trim();
+    i=s.substring(s.indexOf(' '),s.lastIndexOf(' ')).trim();
+    o=s.substring(s.lastIndexOf(' ')).trim();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Введите Ф.И.О: ");
+        String input = new Scanner(System.in).nextLine();
+        input=input.trim();
+        if (!valString(input.trim())) {
+            System.out.println("Неккорректный ввод!");
+        } else {
+            System.out.println("Ваши Ф.И.О:");
+            getFIO(input);
+            System.out.println(f);
+            System.out.println(i);
+            System.out.println(o);
+        }
+
+    }
+
+
 }
